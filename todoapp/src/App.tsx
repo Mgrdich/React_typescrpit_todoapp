68b4ce0c37383f8f98eff072364fa6101e6e984d
Mgrdich <mgotm13@gmail.com>
2019-07-06 16:19:20 +0400

    adding useState with the Genenrics

--- todoapp/src/App.tsx
+++ todoapp/src/App.tsx
@@ -1,17 +1,34 @@
 import React, {useState} from 'react';
 
+
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+interface ITodo {
+    text: string,
+    complete: boolean
+}
+
 function App(): JSX.Element {
     const [value, setValue] = useState<string>("");
-    const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    const [todos, setTodos] = useState<ITodo[]>([]);
 
+    const handleSubmit = function (e: FormElem): void {
+        e.preventDefault();
+        addTodo(value);
+        setValue("");//to get it reinitialize itself
+
+    };
+    const addTodo = function (text: string):void {
+        const newTodos:ITodo[] = [...todos,{text,complete:false}];
+        setTodos(newTodos);
     };
     return (
         <>
             <div>
                 <h1>Todo List</h1>
-                <form action="">
-                    <input type="text" required className="form-control" value={value} onChange={e => setValue(e.target.value)}/>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" required className="form-control" value={value}
+                           onChange={e => setValue(e.target.value)}/>
                     <button type="submit" className="btn btn-info mt-2">Add Todo</button>
                 </form>
             </div>
